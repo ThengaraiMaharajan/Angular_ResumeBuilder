2d42f48d0470b0358939d3638238d4568fa3c05a
THENGARAIMAHARA\theng <tmaharajan1999@gmail.com>
2024-04-19 00:46:28 +0530

    reactive form and form array operations complete

--- Front End/FrontEnd/src/app/app.module.ts
+++ Front End/FrontEnd/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResumeLayerComponent } from './components/resume-layer/resume-layer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { ResumeLayerComponent } from './components/resume-layer/resume-layer.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
